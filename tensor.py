3b6734ba352a47762e4daa93f75db305899f72d6
Theodore Chiu <theochiu.me@gmail.com>
2019-12-16 16:57:52 -0800

    long time ago

---
+++ tensor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from __future__ import absolute_import, division, print_function
+
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
